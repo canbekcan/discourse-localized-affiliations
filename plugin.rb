b6c0b56299ad6fb370ed9c6194a6d64b5dd1117b
canbekcan <can@canbekcan.com>
2026-09-11 23:06:29 +0300

    .

--- plugin.rb
+++ plugin.rb
@@ -2,7 +2,7 @@
 
 # name: discourse-localized-affiliations
 # about: Automatically resolves and applies a user's institutional affiliation based on their email domain, with multi-language support
-# version: 1.0.1
+# version: 1.2.0
 # authors: Can Bekcan
 # url: https://github.com/canbekcan/discourse-localized-affiliations
 
@@ -43,14 +43,15 @@ after_initialize do
       find_affiliation_field
     end
 
-    def self.resolve_institution(email)
+    # Sadece sitenin varsayılan dil dosyasına bakarak domainin geçerli olup olmadığını onaylar
+    def self.configured_domain(email)
       return nil if email.blank?
 
       domain = email.to_s.split("@").last.to_s.downcase
-      institutions = I18n.t("localized.institutions", default: {})
+      institutions = I18n.t("localized.institutions", locale: SiteSetting.default_locale, default: {})
       return nil unless institutions.is_a?(Hash)
 
-      (institutions[domain.to_sym] || institutions[domain]).presence
+      (institutions.key?(domain.to_sym) || institutions.key?(domain.to_s)) ? domain : nil
     end
 
     def self.update_user_affiliation(user, field = nil)
@@ -60,7 +61,9 @@ after_initialize do
       return if field.blank?
 
       primary_email = user.primary_email&.email || user.email
-      target_value = resolve_institution(primary_email)
+      
+      # Çevrilmiş metin YERİNE, kurumun domain'ini (örn: emu.edu.tr) DB'ye kaydediyoruz
+      target_value = configured_domain(primary_email)
       field_key = "user_field_#{field.id}"
 
       if user.custom_fields[field_key] != target_value
@@ -81,7 +84,7 @@ after_initialize do
     end
   end
 
-  # --- Initial setup: runs conditionally on every boot / rebuild / restart ---
+  # --- Initial setup ---
   if SiteSetting.localized_affiliations_enabled
     affiliation_field = ::DiscourseLocalizedAffiliations.ensure_affiliation_field!
     ::DiscourseLocalizedAffiliations.sync_all_existing_users!(affiliation_field)
@@ -93,7 +96,7 @@ after_initialize do
     ::DiscourseLocalizedAffiliations.update_user_affiliation(user)
   end
 
-  # --- Trigger: PRIMARY email changed specifically ---
+  # --- Trigger: PRIMARY email changed specifically & Multi-lang Serializers ---
   reloadable_patch do |plugin|
     UserEmail.class_eval do
       after_save :localized_affiliations_sync_on_primary_change
@@ -105,9 +108,58 @@ after_initialize do
         ::DiscourseLocalizedAffiliations.update_user_affiliation(user)
       end
     end
+
+    # DİNAMİK ÇEVİRİ KATMANI 1: Üye profilindeki "kurum değeri" çevirisi
+    [::UserSerializer, ::UserCardSerializer].each do |klass|
+      klass.class_eval do
+        def user_fields
+          fields = object.user_fields
+          return fields if fields.blank?
+          
+          # Orijinal belleği bozmamak için kopyalıyoruz
+          fields = fields.dup 
+          
+          affiliation_field = ::DiscourseLocalizedAffiliations.find_affiliation_field
+          if affiliation_field
+            field_id_str = affiliation_field.id.to_s
+            domain = fields[field_id_str]
+            
+            if domain.present?
+              institutions = I18n.t("localized.institutions", default: {})
+              if institutions.is_a?(Hash)
+                translated_name = institutions[domain.to_sym] || institutions[domain.to_s]
+                # Arayüze domain yerine, o anki kullanıcının dilindeki karşılığını gönderiyoruz
+                fields[field_id_str] = translated_name if translated_name.present?
+              end
+            end
+          end
+          
+          fields
+        end
+      end
+    end
+
+    # DİNAMİK ÇEVİRİ KATMANI 2: Alanın arayüzdeki başlığı (name) ve açıklaması (description)
+    ::UserFieldSerializer.class_eval do
+      def name
+        if object.name.to_s.downcase.in?(::DiscourseLocalizedAffiliations::AFFILIATION_FIELD_NAMES)
+          I18n.t("localized.field_name", default: object.name)
+        else
+          object.name
+        end
+      end
+
+      def description
+        if object.name.to_s.downcase.in?(::DiscourseLocalizedAffiliations::AFFILIATION_FIELD_NAMES)
+          I18n.t("localized.field_description", default: object.description)
+        else
+          object.description
+        end
+      end
+    end
   end
 
-  # --- Trigger: plugin reactivated (site setting toggled back on) ---
+  # --- Trigger: plugin reactivated ---
   on(:site_setting_changed) do |name, old_value, new_value|
     next unless name.to_s == "localized_affiliations_enabled"
     next unless new_value == true && old_value != true
